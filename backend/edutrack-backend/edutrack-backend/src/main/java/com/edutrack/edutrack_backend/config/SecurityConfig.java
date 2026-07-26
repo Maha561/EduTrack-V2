4596e015119a12bbc1d07e186c653092e38e7a45
Mahalakshmi Anandan <mahalakshmianandan04@gmail.com>
2026-07-26 12:56:57 +0530

    Update SecurityConfig.java

--- backend/edutrack-backend/edutrack-backend/src/main/java/com/edutrack/edutrack_backend/config/SecurityConfig.java
+++ backend/edutrack-backend/edutrack-backend/src/main/java/com/edutrack/edutrack_backend/config/SecurityConfig.java
@@ -1,26 +1,23 @@
 package com.edutrack.edutrack_backend.config;
-import org.springframework.context.annotation.Configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-     @Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> {})
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
 
-   http
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll()
-        );
-
-    return http.build();
-}
-
+        return http.build();
+    }
 }
